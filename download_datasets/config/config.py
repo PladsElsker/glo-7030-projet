ba57f20b16690469540dce414e787bab6766e3d7
Plads <lumoximax2@gmail.com>
2025-03-04 23:03:15 -0500

    rename attributes

--- download_datasets/config/config.py
+++ download_datasets/config/config.py
@@ -8,22 +8,22 @@ from ..downloader import DOWNLOADER_MAP
 
 class DatasetConfig:
     def __init__(self, json: dict) -> None:
-        self.datasets = {}
+        self.downloaders = {}
         for k, v in json.items():
             try:
-                downloader_attribute = v[DOWNLOADER_TYPE_ATTRIBUTE]
+                downloader_type = v[DOWNLOADER_TYPE_ATTRIBUTE]
             except KeyError:
                 raise ConfigError(DOWNLOADER_TYPE_ATTRIBUTE, k)
             
             try:
-                self.datasets[k] = DOWNLOADER_MAP[downloader_attribute](v, k)
+                self.downloaders[k] = DOWNLOADER_MAP[downloader_type](v, k)
             except KeyError:
-                raise DownloaderConfigError(downloader_attribute, k)
+                raise DownloaderConfigError(downloader_type, k)
 
     def download(self, save_to: Path) -> None:
         if not os.path.exists(save_to):
             os.mkdir(save_to)
 
-        for _, downloader in self.datasets.items():
+        for _, downloader in self.downloaders.items():
             downloader.download(save_to)
             downloader.postprocess(save_to)
