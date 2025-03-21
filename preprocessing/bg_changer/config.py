3dedb68c3f80471c4baa08cf5f8a3220916248bd
Bertrand Awenze <beawe@ulaval.ca>
2025-03-21 19:18:26 -0400

    refactor: refactor

--- preprocessing/bg_changer/config.py
+++ preprocessing/bg_changer/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Any, Final
 
 import numpy as np
@@ -5,4 +6,7 @@ import numpy as np
 LOWER_GREEN = np.array([40, 50, 50])
 UPPER_GREEN = np.array([90, 255, 255])
 
-DEFAULT_CONFIG: Final[dict[str, Any]] = {}
+DEFAULT_CONFIG: Final[dict[str, Any]] = {
+    "background_dir_path": Path(__file__).parent.parent.parent / "data" / "backgrounds",
+    "is_autosave_activated": False,
+}
